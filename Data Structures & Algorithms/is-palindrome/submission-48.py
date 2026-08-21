15b7de2692f95c1db7b1373f35eac596bbde7798
EmilioVelazquezCarter <evelazquezca@umass.edu>
2026-08-20 23:57:15 -0400

    Add: is-palindrome - submission-48

---
+++ Data Structures & Algorithms/is-palindrome/submission-48.py
@@ -0,0 +1,82 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) -1
+
+        while l < r:
+            while l < r and not s[l].isalnum(): 
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # l, r = 0, len(s) -1
+
+        # while l < r:
+        #     while l < r and not s[l].isalnum(): 
+        #         l += 1
+        #     while l < r and not s[r].isalnum():
+        #         r -= 1
+        #     if s[l].lower() != s[r].lower():
+        #         return False
+        #     l += 1
+        #     r -= 1
+        # return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # # newS = s
+        # # newS = "".join(c for c in newS if c.isalnum())
+        # # newS = newS.replace(" ", "")
+        # # newS = newS.lower()
+        # # print(newS, newS[::-1])
+        
+
+        # # return newS == newS[::-1]
+
+
+
